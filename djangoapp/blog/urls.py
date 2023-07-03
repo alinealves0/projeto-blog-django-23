38b2439346a41dbe48ba6e7ae60f91e7f3d9d78b
Aline Alves <alinealves0@hotmail.com>
2023-07-03 15:57:17 -0300

    alterações

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,9 +1,12 @@
+from blog.views import index, post, page
 from django.urls import path
-from blog.views import index
+
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('post/', post, name='post'),
+    path('page/', page, name='page'),
 ]
 
