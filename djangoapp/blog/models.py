9e8a516605ec65e5ed4e719b31990f81424948a0
Aline Alves <alinealves0@hotmail.com>
2023-07-04 17:17:50 -0300

    alterações

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        unique=True, default=None, 
+        null=True, blank=True, max_length=255,
+    )
+
+    def save(self,*args, **kwargs):
+        if not self.slug:
+            self.slug = 'nova_slug'
+        return super().save(*args, **kwargs)
