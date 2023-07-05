13d22458fd4117fb7df2b8a20f8391fccec5d2e3
Aline Alves <alinealves0@hotmail.com>
2023-07-05 15:42:00 -0300

    alterações

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,5 +1,6 @@
+from blog.models import Category, Page, Tag 
 from django.contrib import admin
-from blog.models import Tag
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -10,4 +11,29 @@ class TagAdmin(admin.ModelAdmin):
     ordering = '-id',
     prepopulated_fields = {
         'slug': ('name',),
+    }
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'slug', 
+    list_display_links = 'name',
+    search_fields = 'id', 'name', 'slug',
+    list_per_page = 10
+    ordering = '-id',
+    prepopulated_fields = {
+        'slug': ('name',),
+    }
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'id_published', 
+    list_display_links = 'title',
+    search_fields = 'id', 'slug', 'title', 'content',
+    list_per_page = 50
+    list_filter = 'is_published',
+    list_editable = 'is_published',
+    ordering = '-id',
+    prepopulated_fields = {
+        'slug': ('title',),
     }
